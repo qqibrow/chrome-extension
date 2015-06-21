89d8be9d236dcaf2323fbfd3d8f38bfe0311efa6
qqibrow <qqibrow@gmail.com>
2015-06-20 17:43:58 -0700

    fix typo

--- background.js
+++ background.js
@@ -2,16 +2,16 @@
  * Created by lniu on 6/20/15.
  */
 
-function getOuthUrl() {
+function getOauthUrl() {
     return "https://stackexchange.com/oauth/dialog?client_id=4716&redirect_uri=" + chrome.identity.getRedirectURL();
 }
 
 var authorizationCallback = function(data) {
-    console.log("auth succeed");
+    console.log("oauth succeed.");
     console.log(data);
 }
 
-chrome.identity.launchWebAuthFlow({'url' : getOuthUrl(), 'interactive': true }, authorizationCallback);
+chrome.identity.launchWebAuthFlow({'url' : getOauthUrl(), 'interactive': true }, authorizationCallback);
 
 
 
